c3506a7a20a57a0f027c7c99526412abd093fb9d
mkhr03 <mickey_03@icloud.com>
2026-05-04 17:36:32 +0900

    正規分布の問題を一つ追加

--- menu_data.js
+++ menu_data.js
@@ -26,7 +26,8 @@ const menuData = [
         hasSubMenu: true,
         subItems: [
             { name: "二項分布", genre: "binomial" },
-            { name: "幾何分布", genre: "geo" }
+            { name: "幾何分布", genre: "geo" },
+            { name: "正規分布", genre: "normal" }
         ]
     }
 ];
